888134fe363105beb38331d0dc2dff2911a72cad
Arkansas Tom Jones <atte.kuosmanen@edu.savonia.fi>
2018-03-02 05:48:40 +0200

    Add files via upload

---
+++ esx_propaganda/client/main.lua
@@ -0,0 +1,162 @@
+
+local playerData			  = nil
+ESX                           = nil
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(1)
+	end
+	
+	closeGui() --close on client connect
+end)
+
+RegisterNetEvent('esx:playerLoaded') --get xPlayer
+AddEventHandler('esx:playerLoaded', function(xPlayer)
+  playerData = xPlayer
+end)
+
+
+function DisplayHelpText(str) --global function
+	BeginTextCommandDisplayHelp("STRING")
+	AddTextComponentScaleform(str)
+	EndTextCommandDisplayHelp(0, 0, 1, -1)
+end
+
+-- Create Blips
+Citizen.CreateThread(function()
+		
+    local blip = AddBlipForCoord(-1116.89, -502.78, 35.01)
+
+    SetBlipSprite (blip, 184)
+    SetBlipDisplay(blip, 4)
+    SetBlipScale  (blip, 0.9)
+    SetBlipColour (blip, 1)
+    SetBlipAsShortRange(blip, true)
+    
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString("Journalistin toimisto")
+    EndTextCommandSetBlipName(blip)
+
+end)
+
+
+Citizen.CreateThread(function ()
+  
+  while true do
+  
+		Citizen.Wait(1)
+		
+		if CurrentAction ~= nil and once == false then
+			
+			local _job = tostring(exports['esx_policejob']:getJob())
+			
+			if(_job == "reporter") then 
+				DisplayHelpText("Paina ~INPUT_CONTEXT~ lunastaaksesi palkka")
+
+			  if IsControlPressed(0, Keys['E']) and (GetGameTimer() - GUI.Time) > 300 then
+				
+				TriggerServerEvent('esx_propaganda:getPaid', "ok")
+				once = true
+				CurrentAction = nil
+			  end
+			end
+			
+		end
+	
+	end
+	
+end)
+
+AddEventHandler('esx_propaganda:hasEnteredMarker', function (zone)
+	
+	once = false
+	CurrentAction = 'pay_menu'
+
+end)
+
+AddEventHandler('esx_propaganda:hasExitedMarker', function (zone)
+	
+	CurrentAction = nil
+
+end)
+
+-- Display markers
+Citizen.CreateThread(function()
+	while true do
+		
+		Wait(5)
+		
+		local playerPed = GetPlayerPed(-1)
+		local coords    = GetEntityCoords(playerPed)
+
+		if(GetDistanceBetweenCoords(coords, -1116.89, -502.78, 35.01, true) < 100 ) then
+			DrawMarker(1, -1116.89, -502.78, 35.01, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 1.0, 1.0, 1.0, 255, 0, 5, 100, false, true, 2, false, false, false, false)
+		end	
+
+	end
+end)
+
+
+-- Enter / Exit marker events
+Citizen.CreateThread(function ()
+  while true do
+    Wait(5)
+
+    local coords      = GetEntityCoords(GetPlayerPed(-1))
+    local isInMarker  = false
+	
+		if(GetDistanceBetweenCoords(coords, -1116.89, -502.78, 35.01, true) < 1.0) then
+	      isInMarker  = true
+		end
+
+
+		if (isInMarker and not HasAlreadyEnteredMarker) then
+		  HasAlreadyEnteredMarker = true
+		  TriggerEvent('esx_propaganda:hasEnteredMarker', 'not in use')
+		end
+
+		if not isInMarker and HasAlreadyEnteredMarker then
+		  HasAlreadyEnteredMarker = false
+		  TriggerEvent('esx_propaganda:hasExitedMarker', 'not in use')
+		end
+  end
+end)
+
+
+-- Open Gui and Focus NUI
+function openGui()
+  SendNUIMessage({openPropaganda = true})
+  Citizen.CreateThread(function()
+	Citizen.Wait(500)
+	SetNuiFocus(true, true)
+  end)
+end
+
+-- Close Gui and disable NUI
+
+function closeGui()
+  SetNuiFocus(false)
+  SendNUIMessage({openPropaganda = false})
+end
+
+-- NUI Callback Methods
+RegisterNUICallback('closePropaganda', function(data, cb)
+  closeGui()
+  cb('ok')
+end)
+
+RegisterNUICallback('postArticle', function(data, cb)
+  
+  TriggerServerEvent('esx_propaganda:postArticle', data)
+  
+  cb('ok')
+end)
+
+
+
+function openPropaganda ()
+	openGui()
+end
+
+
---
+++ esx_propaganda/server/main.lua
@@ -0,0 +1,106 @@
+ESX              = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+TriggerEvent('esx_phone:registerNumber', 'reporter', "~b~Uutisvinkki ~w~", true, true)
+
+RegisterServerEvent('esx_propaganda:getPaid')
+AddEventHandler('esx_propaganda:getPaid', function(data)
+	
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	
+	MySQL.Async.fetchAll(
+	'SELECT * FROM paychecks WHERE receiver = @identifier', 
+	{
+      ['@identifier'] = xPlayer.identifier
+    }, 
+	function(rows)
+	
+		local topay = rows
+		if #topay > 0 then 
+			
+			MySQL.Async.fetchAll(
+			'DELETE FROM paychecks WHERE receiver = @identifier', 
+			{
+			  ['@identifier'] = xPlayer.identifier
+			}, function(rowschanged)
+				
+				local amount = 0
+				
+				for i=1, #topay, 1 do
+					
+					amount = amount + rows[i].amount
+					
+				end
+				TriggerClientEvent('esx:showNotification', xPlayer.source, "~g~ Sait palkkaa " .. amount)
+				xPlayer.addMoney(amount)
+			end
+			)
+			
+		else
+			TriggerClientEvent('esx:showNotification', xPlayer.source, "~r~ Kukaan ei ole tykännyt artikkeleistasi :(")
+		end
+	end
+	)
+
+end)
+
+RegisterServerEvent('esx_propaganda:postArticle')
+AddEventHandler('esx_propaganda:postArticle', function(data)
+	
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+	
+	
+	MySQL.Async.fetchAll(
+	'SELECT * FROM users WHERE identifier = @identifier', 
+	{
+      ['@identifier'] = xPlayer.identifier
+    }, 
+	function(rows)
+		
+		local _name = "Matti Meikäläinen"
+		
+		for i = 1, #rows, 1 do
+			_name = rows[i].firstname .. ' ' .. rows[i].lastname
+		end
+		
+		MySQL.Async.fetchAll(
+		'INSERT INTO news_main (title, bait_title, content, author_name, author_id, news_type, imgurl) VALUES (@title, @bait, @content, @author_name, @uid, @type, @url)',
+		{ 
+		  ['@title'] = data.title,
+		  ['@bait'] = data.bait_title,
+		  ['@content'] = data.content,
+		  ['@author_name'] = _name,
+		  ['@uid'] = xPlayer.identifier,
+		  ['@type'] = data.type,
+		  ['@url'] = data.imgurl,
+		},
+		function (rows_changed)
+		  TriggerClientEvent('esx:showNotification', xPlayer.source, "~g~ Artikkeli lisätty!")
+		end
+		)
+	end
+	)
+	
+	MySQL.Async.execute(
+	'DELETE FROM news_main WHERE id NOT IN (SELECT id FROM (SELECT id FROM news_main ORDER BY id DESC LIMIT 10 ) foo)', 
+	{},
+	function(rows2)
+		TriggerClientEvent('esx:showNotification', xPlayer.source, "~g~ Tykkäykset päivitetty!")
+	end
+	)
+	
+	
+	MySQL.Async.execute(
+	'DELETE FROM news_likes WHERE news_id NOT IN (SELECT id FROM (SELECT id FROM news_main ORDER BY id DESC LIMIT 10 ) foo)',
+	{},
+	function(rows3)
+		TriggerClientEvent('esx:showNotification', xPlayer.source, "~g~ Vanhat artikkelit poistettu!")
+	end
+	)
+	
+end)
+
+
